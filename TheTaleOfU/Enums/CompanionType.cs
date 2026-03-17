a6c5c237d5d2f2fca34a88f210a1bebb2e9826d1
taylorjrsmith <42411334+taylorjrsmith@users.noreply.github.com>
2026-03-17 15:04:54 +0000

    refactor: modernise CompanionType.cs, add initial enum values

--- TheTaleOfU/Enums/CompanionType.cs
+++ TheTaleOfU/Enums/CompanionType.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace TheTaleOfU.Enums;
 
-namespace TheTaleOfU.Enums
+public enum CompanionType
 {
-    public enum CompanionType
-    {
-    }
+    Dog,
+    Cat,
+    Robot,
+    Wizard
 }
