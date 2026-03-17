67633c38fc3727ca4bd1d22c32ca9e14ca67aa40
taylorjrsmith <42411334+taylorjrsmith@users.noreply.github.com>
2026-03-17 14:58:56 +0000

    refactor: pass DbContext through RunScenario, use EF Core, nullable annotations

--- TheTaleOfU/Scenario.cs
+++ TheTaleOfU/Scenario.cs
@@ -1,122 +1,113 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using TheTaleOfU.Enums;
 
-namespace TheTaleOfU
+namespace TheTaleOfU;
+
+public class Scenario
 {
-    public class Scenario
+    public int Id { get; set; }
+    public string ScenarioDescription { get; set; } = string.Empty;
+    public PlayerType AllowedPlayerTypes { get; set; }
+    public List<Option> Options { get; set; } = new();
+    public bool IsEndOfScenarioRoute { get; set; }
+    public string? EndOfEventMethodName { get; set; }
+
+    public bool HasEvent => !string.IsNullOrEmpty(EndOfEventMethodName);
+
+    private Player? CurrentPlayer { get; set; }
+
+    /// <summary>
+    /// The event that can be triggered at the end of this scenario (gain item, take damage, etc.)
+    /// </summary>
+    public virtual ScenarioEvent? Event { get; set; }
+
+    public void RunScenario(Player player, TheTaleOfUContext context)
     {
-        public int Id { get; set; }
-        public string ScenarioDescription { get; set; }
-        public PlayerType AllowedPlayerTypes { get; set; }
-        public List<Option> Options { get; set; }
-        public bool IsEndOfScenarioRoute { get; set; }
-        public string EndOfEventMethodName { get; set; }
-        [NotMapped]
-        public bool HasEvent => !string.IsNullOrEmpty(EndOfEventMethodName);
-        [NotMapped]
-        private Player CurrentPlayer { get; set; }
-        /// <summary>
-        /// The linked item is either an item that can be gained from this scenario or an item that can be used in this scenario
-        /// </summary>
-        public virtual ScenarioEvent Event { get; set; }
+        CurrentPlayer = player;
+        Console.WriteLine(ScenarioDescription);
 
-        public void RunScenario(Player player)
+        if (IsEndOfScenarioRoute || HasEvent)
         {
-            CurrentPlayer = player;
-            Console.WriteLine(ScenarioDescription);
-            if (IsEndOfScenarioRoute || HasEvent)
-            {
-                Event = new TheTaleOfUContext().Events.FirstOrDefault(a => a.OriginScenarioId == Id);
-                if(Event!=null)
+            Event = context.Events.Include(e => e.LinkedItem)
+                                  .FirstOrDefault(a => a.OriginScenarioId == Id);
+            if (Event is not null && EndOfEventMethodName is not null)
                 CallEventFromString(EndOfEventMethodName);
-            }
+        }
 
-            if (Options == null)
-                Options = new TheTaleOfUContext().Options.Where(a => a.OriginScenarioId == Id).ToList();
+        if (Options.Count == 0)
+            Options = context.Options.Where(a => a.OriginScenarioId == Id).ToList();
 
+        if (Options.Count == 0 || IsEndOfScenarioRoute)
+        {
+            Console.WriteLine("--- End of scenario route ---");
             Console.ReadKey();
-            foreach (var o in Options)
-            {
-                Console.WriteLine($"{o.OptionIdentifier} : {o.Text}");
-            }
-
-            string option = "";
-            var wasValidOption = false;
-            while (!wasValidOption)
-            {
-                option = Console.ReadLine();
-                wasValidOption = ProcessOption(option);
-                if (!wasValidOption)
-                    Console.WriteLine("Please enter a valid option number");
-            }
-            var numericOption = Convert.ToInt32(option);
-
-            var nextOption = Options.FirstOrDefault(a => a.OptionIdentifier == numericOption);
-            var scenario = new TheTaleOfUContext().Scenarios.FirstOrDefault(a => a.Id == nextOption.NextScenarioId);
-            nextOption.NextScenario = scenario;
-            if (nextOption.NextScenario == null)
-                return;
-            nextOption.NextScenario.RunScenario(player);
-
+            return;
         }
 
-        public bool ProcessOption(string selectedOption)
-        {
-            var strippedOption = selectedOption.Trim();
+        Console.ReadKey();
+        foreach (var o in Options)
+            Console.WriteLine($"{o.OptionIdentifier} : {o.Text}");
 
-            if (!Options.Any(a => a.OptionIdentifier == Convert.ToInt32(strippedOption)))
-            {
-                return false;
-            }
-            return true;
+        string? option;
+        bool wasValidOption = false;
+        while (!wasValidOption)
+        {
+            option = Console.ReadLine();
+            wasValidOption = ProcessOption(option);
+            if (!wasValidOption)
+                Console.WriteLine("Please enter a valid option number.");
         }
 
+        option = Console.ReadLine();
+        var numericOption = Convert.ToInt32(option);
+        var nextOption = Options.First(a => a.OptionIdentifier == numericOption);
+        var nextScenario = context.Scenarios.FirstOrDefault(a => a.Id == nextOption.NextScenarioId);
+        if (nextScenario is null) return;
 
-        public void CallEventFromString(string eventName)
-        {
-            MethodInfo mi = this.GetType().GetMethod(eventName);
-            if (mi != null)
-                mi.Invoke(this, null);
-        }
+        nextOption.NextScenario = nextScenario;
+        nextOption.NextScenario.RunScenario(player, context);
+    }
 
-        public void UseShrinkRay()
-        {
+    public bool ProcessOption(string? selectedOption)
+    {
+        if (!int.TryParse(selectedOption?.Trim(), out int parsed)) return false;
+        return Options.Any(a => a.OptionIdentifier == parsed);
+    }
 
-        }
+    public void CallEventFromString(string eventName)
+    {
+        var mi = GetType().GetMethod(eventName);
+        mi?.Invoke(this, null);
+    }
 
-        public void UseAnItem()
-        {
-            if (CurrentPlayer.ActiveItem.Durability > 0)
-            {
-                CurrentPlayer.ActiveItem.Durability--;
-            }
-            if (CurrentPlayer.ActiveItem.Durability == 0)
-                CurrentPlayer.Inventory.Remove(CurrentPlayer.ActiveItem);
-        }
+    public void UseAnItem()
+    {
+        if (CurrentPlayer?.ActiveItem is null) return;
+        if (CurrentPlayer.ActiveItem.Durability > 0)
+            CurrentPlayer.ActiveItem.Durability--;
+        if (CurrentPlayer.ActiveItem.Durability == 0)
+            CurrentPlayer.Inventory.Remove(CurrentPlayer.ActiveItem);
+    }
 
-        public void GainItem()
-        {
-            CurrentPlayer.Inventory.Add(Event.LinkedItem);
-        }
+    public void GainItem()
+    {
+        if (Event?.LinkedItem is not null)
+            CurrentPlayer?.Inventory.Add(Event.LinkedItem);
+    }
 
-        public void TakeDamage()
-        {
+    public void TakeDamage()
+    {
+        if (CurrentPlayer is not null && Event?.LinkedItem is not null)
             CurrentPlayer.Health -= Event.LinkedItem.Value;
-        }
+    }
 
-
-
-        public void HealthGain()
+    public void HealthGain()
+    {
+        if (CurrentPlayer is not null && Event?.LinkedItem is not null)
         {
             CurrentPlayer.Health += Event.LinkedItem.Value;
-            Console.WriteLine($"Congratulations {CurrentPlayer.Name} you have gained {Event.LinkedItem.Value} health for using a {Event.LinkedItem.Name}");
+            Console.WriteLine($"Congratulations {CurrentPlayer.Name}, you gained {Event.LinkedItem.Value} health from using a {Event.LinkedItem.Name}!");
         }
-
     }
 }
