7be06e87eee7e5eb1574c69a1026e1c7208bd51c
taylorjrsmith <42411334+taylorjrsmith@users.noreply.github.com>
2026-03-17 14:58:55 +0000

    refactor: update GameEngine to use DI-injected DbContext, .NET 10 patterns

--- TheTaleOfU/Startup.cs
+++ TheTaleOfU/Startup.cs
@@ -1,88 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TheTaleOfU.Enums;
 
-namespace TheTaleOfU
+namespace TheTaleOfU;
+
+public class GameEngine
 {
-    public class GameEngine
+    private readonly TheTaleOfUContext _context;
+    public List<Player> Players { get; set; } = new();
+
+    public GameEngine(TheTaleOfUContext context)
     {
-        public List<Player> Players { get; set; } = new List<Player>();
+        _context = context;
+    }
 
-        public void StartGame()
+    public void StartGame()
+    {
+        Console.WriteLine("Welcome to TheLifeOfU");
+        Console.WriteLine("How many of you are playing?");
+
+        int playerCountNumeric = 0;
+        bool isValid = false;
+        while (!isValid)
         {
-            Console.WriteLine("Welcome to TheLifeOfU");
-            Console.WriteLine("How many of you are playing?");
-            var isValid = false;
-            int playerCountNumeric = 0;
-            while (!isValid)
+            var playerCount = Console.ReadLine();
+            isValid = ValidatePlayerCount(playerCount, out playerCountNumeric);
+        }
+
+        for (int i = 0; i < playerCountNumeric; i++)
+        {
+            var player = new Player { Health = 100, Inventory = new List<Item>() };
+            Console.WriteLine($"Player {i + 1}, what is your name?");
+            player.Name = Console.ReadLine() ?? $"Player {i + 1}";
+            GeneratePlayerType(player);
+            Console.WriteLine($"Welcome to TheLifeOfU {player.Name}, you have been assigned as a {player.PlayerType}. Press any key to continue.");
+            Console.ReadKey();
+            Players.Add(player);
+        }
+
+        Console.WriteLine("This round of TheLifeOfU will last 1 hour. Earn as much as you can, don't die, and run through as many scenarios as possible. Good luck!");
+        var hourWindow = DateTime.Now.AddHours(1);
+
+        while (hourWindow > DateTime.Now)
+        {
+            foreach (var p in Players)
             {
-                var playerCount = Console.ReadLine();
-                isValid = ValidatePlayerCount(playerCount);
-                if (isValid)
-                    playerCountNumeric = Convert.ToInt32(playerCount);
-            }
-
-            for (int i = 0; i < playerCountNumeric; i++)
-            {
-                var player = new Player();
-                player.Health = 100;
-                Console.WriteLine($"Player {i}, what is your name?");
-                var name = Console.ReadLine();
-                player.Name = name;
-                player.Inventory = new List<Item>();
-                GeneratePlayerType(player);
-                Console.WriteLine($"Welcome to TheLifeOfU {player.Name}, I have assigned you as a {player.PlayerType}, Press any key to continue");
-                Console.ReadKey();
-                Players.Add(player);
-
-            }
-
-            Console.WriteLine("This round of TheLifeOfU will last 1 hour, you now have 1 hour to earn as much money as you can not die and run through different scenarios, good luck");
-            var hourWindow = DateTime.Now.AddHours(1);
-
-            while (hourWindow > DateTime.Now)
-            {
-                foreach (var p in Players)
+                Console.WriteLine($"{p.Name}, it's your turn.");
+                var scenario = LoadScenario();
+                if (scenario is null)
                 {
-                    Console.WriteLine($"{p.Name} it's your turn");
-                    var scenario = LoadScenario();
-                    scenario.RunScenario(p);
+                    Console.WriteLine("No scenarios found in the database.");
+                    return;
                 }
-            }
-
-
-        }
-
-        public Scenario LoadScenario()
-        {
-            using (var scenarioContext = new TheTaleOfUContext())
-            {
-                var numberOfScenarios = scenarioContext.Scenarios.Count();
-
-                var scenario = scenarioContext.Scenarios.FirstOrDefault();
-                var options = scenarioContext.Options.Where(a => a.OriginScenarioId == scenario.Id);
-                scenario.Options = options.ToList();
-                return scenario;
+                scenario.RunScenario(p, _context);
             }
         }
+    }
 
-        public void GeneratePlayerType(Player player)
-        {
-            var enums = Enum.GetValues(typeof(PlayerType));
-            var numberOfPlayerTypes = enums.Length;
-            var randomNumber = new Random().Next(1, numberOfPlayerTypes);
-            var playerType = enums.GetValue(randomNumber);
-            player.PlayerType = (PlayerType)playerType;
-        }
+    public Scenario? LoadScenario()
+    {
+        var scenario = _context.Scenarios.FirstOrDefault();
+        if (scenario is null) return null;
+        scenario.Options = _context.Options.Where(a => a.OriginScenarioId == scenario.Id).ToList();
+        return scenario;
+    }
 
-        public bool ValidatePlayerCount(string playerCount)
-        {
-            var isValidNumber = int.TryParse(playerCount, out int numericCount);
+    public void GeneratePlayerType(Player player)
+    {
+        var values = Enum.GetValues<PlayerType>();
+        player.PlayerType = values[new Random().Next(0, values.Length)];
+    }
 
-            return isValidNumber;
-        }
+    public bool ValidatePlayerCount(string? input, out int count)
+    {
+        return int.TryParse(input, out count) && count > 0;
     }
 }
