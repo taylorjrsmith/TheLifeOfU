92658494b7380333c388d4163da2b88baa17e1f0
taylorjrsmith <42411334+taylorjrsmith@users.noreply.github.com>
2026-03-17 14:58:58 +0000

    refactor: nullable annotations for Item.cs

--- TheTaleOfU/Item.cs
+++ TheTaleOfU/Item.cs
@@ -1,23 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TheTaleOfU.Enums;
 
-namespace TheTaleOfU
+namespace TheTaleOfU;
+
+public class Item
 {
-    public class Item
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public ItemType Type { get; set; }
-        public int Damage { get; set; }
-        public string Effect { get; set; }
-        /// <summary>
-        /// Durability is the number of uses an item has
-        /// </summary>
-        public int Durability { get; set; }
-        public int Value { get; set; }
-    }
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public ItemType Type { get; set; }
+    public int Damage { get; set; }
+    public string? Effect { get; set; }
+    /// <summary>Number of uses remaining.</summary>
+    public int Durability { get; set; }
+    public int Value { get; set; }
 }
