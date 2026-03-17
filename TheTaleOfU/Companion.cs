53128b5ba8222243fde4b7111a37b359b9e7542d
taylorjrsmith <42411334+taylorjrsmith@users.noreply.github.com>
2026-03-17 14:59:01 +0000

    refactor: modern C# for Companion.cs

--- TheTaleOfU/Companion.cs
+++ TheTaleOfU/Companion.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TheTaleOfU.Enums;
 
-namespace TheTaleOfU
+namespace TheTaleOfU;
+
+public class Companion
 {
-    public class Companion
-    {
-        public int Id { get; set; }
-        public CompanionType CompanionType { get; set; }
-        public List<Item> Inventory { get; set; }
-    }
+    public int Id { get; set; }
+    public CompanionType CompanionType { get; set; }
+    public List<Item> Inventory { get; set; } = new();
 }
