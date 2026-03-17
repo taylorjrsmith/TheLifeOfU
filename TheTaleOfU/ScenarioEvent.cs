04434af7d8b67d702245aff6109cdd6a60dcb63a
taylorjrsmith <42411334+taylorjrsmith@users.noreply.github.com>
2026-03-17 14:59:00 +0000

    refactor: nullable annotations for ScenarioEvent.cs

--- TheTaleOfU/ScenarioEvent.cs
+++ TheTaleOfU/ScenarioEvent.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace TheTaleOfU;
 
-namespace TheTaleOfU
+public class ScenarioEvent
 {
-    public class ScenarioEvent
-    {
-        public int Id { get; set; }
-        public int Value { get; set; }
-        public int OriginScenarioId { get; set; }
-        public virtual Item LinkedItem { get; set; }
-    }
+    public int Id { get; set; }
+    public int Value { get; set; }
+    public int OriginScenarioId { get; set; }
+    public virtual Item? LinkedItem { get; set; }
 }
