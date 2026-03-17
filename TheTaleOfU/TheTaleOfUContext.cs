5bb06bdf0f37f05091eeacfd5699d039554ad202
taylorjrsmith <42411334+taylorjrsmith@users.noreply.github.com>
2026-03-17 14:58:54 +0000

    refactor: migrate DbContext from EF6 to EF Core 9

--- TheTaleOfU/TheTaleOfUContext.cs
+++ TheTaleOfU/TheTaleOfUContext.cs
@@ -1,22 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
-namespace TheTaleOfU
+namespace TheTaleOfU;
+
+public class TheTaleOfUContext : DbContext
 {
-    public class TheTaleOfUContext : DbContext
+    public DbSet<Scenario> Scenarios { get; set; } = null!;
+    public DbSet<Option> Options { get; set; } = null!;
+    public DbSet<Item> Items { get; set; } = null!;
+    public DbSet<ScenarioEvent> Events { get; set; } = null!;
+
+    public TheTaleOfUContext(DbContextOptions<TheTaleOfUContext> options) : base(options)
     {
-        public DbSet<Scenario> Scenarios { get; set; }
-        public DbSet<Option> Options { get; set; }
-        public DbSet<Item> Items { get; set; }
-        public DbSet<ScenarioEvent> Events { get; set; }
-        public TheTaleOfUContext() : base("TheTaleOfUContext")
+    }
+
+    // Parameterless constructor for design-time tooling (migrations)
+    public TheTaleOfUContext() { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
         {
-
+            optionsBuilder.UseSqlServer("Name=ConnectionStrings:TheTaleOfUContext");
         }
+    }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Option>()
+            .HasOne(o => o.OriginScenario)
+            .WithMany(s => s.Options)
+            .HasForeignKey(o => o.OriginScenarioId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Option>()
+            .HasOne(o => o.NextScenario)
+            .WithMany()
+            .HasForeignKey(o => o.NextScenarioId)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
